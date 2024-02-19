4a67ba10f69d8338d4f16a496874f9fda0576a13
pradeeshkumark <pradeeshkumarchn@gmail.com>
2024-02-19 20:28:25 +0530

    test branch commit

---
+++ additionnew1.java
@@ -0,0 +1,8 @@
+public class additionnew1 {
+    public static void main(String[] args) {
+        int a=10000;
+        int b=200;
+        int c=a+b;
+        System.out.println("Value of C:"+c);
+    }
+}
