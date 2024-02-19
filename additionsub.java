f76ca32f81a92a585b747b7590295f3cc95875dd
pradeeshkumark <pradeeshkumarchn@gmail.com>
2024-02-19 14:50:05 +0530

    First commit1

---
+++ additionsub.java
@@ -0,0 +1,13 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class additionsub {
+    public static void main(String[] args) {
+        int a=100;
+        int b=200;
+        int c=a+b;
+        int d=a-b;
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
