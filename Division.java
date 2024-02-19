89a78029c51fb4c3c72ee54412ca17eef87c259b
pradeeshkumark <159707408+pradeeshkumark@users.noreply.github.com>
2024-02-19 15:19:09 +0530

    Division a and b value changed

--- Division.java
+++ Division.java
@@ -1,7 +1,7 @@
 public class division {
         public static void main(String[] args) {
-            int a=20;
-            int b=10;
+            int a=200;
+            int b=100;
             int c=a/b;
             System.out.println(c);
         }
