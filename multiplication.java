890b7a3c3279bfcf7e132959ab66a21271be70af
pradeeshkumark <pradeeshkumarchn@gmail.com>
2024-02-19 14:55:43 +0530

    second commit1

--- multiplication.java
+++ multiplication.java
@@ -1,7 +1,7 @@
 public class multiplication {
     public static void main(String[] args) {
-        int a=20;
-        int b=10;
+        int a=2000;
+        int b=2;
         int c=a*b;
         System.out.println(c);
     }
