08802f2bbd0acb0ffb6596768d9f3ae65c7f6282
pradeeshkumark <pradeeshkumarchn@gmail.com>
2024-02-19 17:09:48 +0530

    Subtraction file changes

--- subtraction.java
+++ subtraction.java
@@ -1,9 +1,11 @@
 public class subtraction {
         public static void main(String[] args) {
-            int a=20;
-            int b=10;
+            int a=2000;
+            int b=1000;
             int c=a-b;
+            int d=c-b;
             System.out.println(c);
+            System.out.println(d);
         }
     }
 
