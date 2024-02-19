c4f4d3c8ec09ee259d038e5d3e51991d8802e173
pradeeshkumark <pradeeshkumarchn@gmail.com>
2024-02-19 19:20:12 +0530

    nebranch

--- multiplication.java
+++
@@ -1,9 +0,0 @@
-public class multiplication {
-    public static void main(String[] args) {
-        int a=2000;
-        int b=2;
-        int c=a*b;
-        System.out.println(c);
-    }
-    }
-}
